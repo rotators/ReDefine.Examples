e9db540489de5a541d663408fc6d16dbc62cf177
Wipe <wipe.git@gmail.com>
2021-06-07 14:16:00 +0200

    fix

--- _Headers/define_trait.h
+++ _Headers/define_trait.h
@@ -1,8 +1,6 @@
-#ifndef __DEFINE_TRAIT__
-#define __DEFINE_TRAIT__
-
-#define TRAIT_PERK        (0)
-#define TRAIT_OBJECT      (1)
-#define TRAIT_TRAIT       (2)
-
-#endif  // __DEFINE_TRAIT__ //
+#ifndef __DEFINE_TRAIT__                    
+#define __DEFINE_TRAIT__                    
+#define TRAIT_PERK        (0)               
+#define TRAIT_OBJECT      (1)               
+#define TRAIT_TRAIT       (2)               
+#endif   //                __DEFINE_TRAIT__  //
